e9a3bb204f367fd0e7b79f55c90a520112c8a5a1
easyvipin <vipinchandra555@gmail.com>
2023-04-24 23:16:10 +0530

    feat:setup layout grid

---
+++ src/Components/Layout.tsx
@@ -0,0 +1,22 @@
+import SideBar from "./layout/Sidebar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen border-2">
+      <div className="container h-full mx-auto xl:px-30">
+        <div className="grid grid-cols-4 h-full border-4 border-red-400">
+          <SideBar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800 ">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/Components/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ISidebarProps {}
+
+const SideBar: React.FC<ISidebarProps> = (props) => {
+  return <div></div>;
+};
+
+export default SideBar;
